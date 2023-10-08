52e88aa59e5fa82e4c29e901157fe7b8489b3a08
codebreaker518 <diegoperezperez518@gmail.com>
2023-10-08 01:41:27 -0600

    feat:3d earth

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-import Image from "next/image";
+'use client'
+
+import { Suspense } from 'react'
+import ThreeScene from './components/ThreeScene'
+import Model from './components/Model'
+
+import { OrbitControls, Stars } from '@react-three/drei'
 
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>asd</div>
-        </main>
-    );
+  return (
+    <main className='h-screen  p-0 m-0'>
+      <ThreeScene>
+        <color attach='background' args={['#00204a']} />
+        <ambientLight intensity={0.3} />
+        <hemisphereLight intensity={1} />
+        <directionalLight intensity={0.5} position={[0, 10, 0]} />
+        <OrbitControls autoRotate />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+        <Stars />
+      </ThreeScene>
+    </main>
+  )
 }
